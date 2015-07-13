c80b5d013870b82e3b58af6b27e497b86143e923
bracharoths <bracha.b.rothschild@gmail.com>
2015-07-12 19:23:33 -0600

    added questions as comments

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-    $('.ryu-cool').hide();
+  $('.ryu').mouseenter(function() { //when mouse enters the .ryu then....
+    $('.ryu-still').hide(); //hide ryu still gif
+    $('.ryu-ready').show(); //show ryu ready gif
+    $('.ryu-cool').hide();// hide ryu cool gif
  })
-  .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-cool').hide();
-	  $('.ryu-still').show();
+  .mouseleave(function() { //when mouse leaves area- do this funtion: 
+    $('.ryu-ready').hide(); //hide ryu ready gif 
+    $('.ryu-cool').hide(); //hide ryu cool gif 
+	  $('.ryu-still').show(); //show try still gif 
  })
-  .mousedown(function() {
-  	playHadouken();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.ryu-cool').hide();
-    $('.hadouken').finish().show().animate(
+  .mousedown(function() { //when ryu is clicked...  
+  	playHadouken(); //playHoadouken funtion (see below)
+    $('.ryu-ready').hide(); // hide ryu ready  gif 
+    $('.ryu-throwing').show(); //show ryu gif throwing 
+    $('.ryu-cool').hide(); //hide ryu cool
+    $('.hadouken').finish().show().animate( //find hadouken, stop animation, hide hadouken, and perform animation according to css properties
   	{'left': '300px'},
   	500,
   	function() {
-    $(this).hide();
-    $(this).css('left', '-212px');
+    $(this).hide(); // hide this? the hadouken? 
+    $(this).css('left', '-212px'); //find css for this? the hadouken? 
   }
  );
 })
-   .mouseup(function() {
+   .mouseup(function() { //when mouse is released.. 
   	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
     $('.ryu-cool').hide();
   });
-  $(document).keydown(function(event){
+  $(document).keydown(function(event){ //when x is pressed 
    	if (event.which===88) {
    		console.log('x')
    		$('.ryu-cool').show();
@@ -38,8 +38,8 @@ $(document).ready(function() {
    	};
    });
 });
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
+function playHadouken () { //playHadouken function is defined
+  $('#hadouken-sound')[0].volume = 0.5; 
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
